9440fdc934d3f5013a73f42860fe190f2d64058d
Charlotte Cullip <charlotte.cullip@gmail.com>
2018-12-11 23:37:00 -0800

    removed a bunch of print statements and crop non-square images

--- model/keras_model.py
+++ model/keras_model.py
@@ -15,125 +15,35 @@ data_format = 'channels_first'
 #RGB to gray
 convert_gray = [0.2989, 0.587, 0.114]
 
-
 def normalize(image):
     image = np.asarray(image, dtype='float32')
     image = image/255
     return image
 
-def KerasModel(params, weights_filename):
-    param_filename = params
-
-    param_list = []
-    with open(param_filename, 'r') as file:
-        all_lines = file.readlines()
-        for j in range(0, len(all_lines)):
-            param_list.append(all_lines[j].rstrip('\n'))
-
-    num_conv_filt1 = int(param_list[5])
-    kernel_size1 = int(param_list[7])
-    num_strides1 = int(param_list[9])
-    activation = ""
-    activation += str(param_list[11])
-    num_conv_filt2 = int(param_list[13])
-    kernel_size2 = int(param_list[15])
-    num_strides2 = int(param_list[17])
-    dense1 = int(param_list[19])
-    dropout1 = float(param_list[21])
-    dropout2 = float(param_list[23])
-
-    model = Sequential()
-    model.add(Conv2D(filters=num_conv_filt1, kernel_size=kernel_size1, strides=(num_strides1), data_format=data_format,
-                     input_shape=(1, 100, 100)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), data_format=data_format))
-    model.add(
-        Conv2D(filters=num_conv_filt2, kernel_size=kernel_size2, strides=(num_strides2), data_format=data_format))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), data_format=data_format))
-    model.add(Flatten())
-    model.add(Dropout(dropout1))
-    model.add(Dense(dense1))
-    model.add(Activation('relu'))
-    model.add(Dropout(dropout2))
-    model.add(Dense(len(classes)))
-    model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-
-    print("loaded weights")
-    model.load_weights(weights_filename)
-    model._make_predict_function()
-
-    return model
-
-
-
 def predict(model, img):
     print("predicting")
     model.predict(img)
 
-def KerasModels(params, weights_filename, image):
-    param_filename = params
-    param_list = []
-    with open(param_filename, 'r') as file:
-        all_lines = file.readlines()
-        for j in range(0, len(all_lines)):
-            param_list.append(all_lines[j].rstrip('\n'))
-
-    num_conv_filt1 = int(param_list[5])
-    kernel_size1 = int(param_list[7])
-    num_strides1 = int(param_list[9])
-    activation = ""
-    activation += str(param_list[11])
-    num_conv_filt2 = int(param_list[13])
-    kernel_size2 = int(param_list[15])
-    num_strides2 = int(param_list[17])
-    dense1 = int(param_list[19])
-    dropout1 = float(param_list[21])
-    dropout2 = float(param_list[23])
-
-    model = Sequential()
-    model.add(Conv2D(filters=num_conv_filt1, kernel_size=kernel_size1, strides=(num_strides1), data_format=data_format,
-                     input_shape=(1, 100, 100)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), data_format=data_format))
-    model.add(Conv2D(filters=num_conv_filt2, kernel_size=kernel_size2, strides=(num_strides2), data_format=data_format))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), data_format=data_format))
-    model.add(Flatten())
-    model.add(Dropout(dropout1))
-    model.add(Dense(dense1))
-    model.add(Activation('relu'))
-    model.add(Dropout(dropout2))
-    model.add(Dense(len(classes)))
-    model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-
-    print("loading weights")
-    model.load_weights(weights_filename)
-    print("loaded weights")
-    preds = model.predict(image)
-    print(preds)
-    return preds
-
 def EntireModel(model_filename, img):
-    print("creating model")
+    # create model
     model = load_model(model_filename)
-    print("scaling img")
+
+    # normalize image
     img = normalize(img)
-    print(img)
-    print("predicting")
+
+    # get predictions
     preds = model.predict(img)
+
+    # get top prediction
     index = np.argmax(preds)
+    pred = classes[index]
+
+    # get second prediction
     preds2 = np.delete(preds, index)
     classes2 = np.delete(classes, index)
     index2 = np.argmax(preds2)
-    pred = classes[index]
     pred2 = classes2[index2]
+
     return pred, pred2
 
 
--- serve.py
+++ serve.py
@@ -1,4 +1,4 @@
-from model.keras_model import KerasModel, KerasModels, EntireModel
+from model.keras_model import EntireModel
 from PIL import Image
 import numpy as np
 from io import BytesIO
@@ -11,41 +11,30 @@ def get_model_api():
     #model = KerasModel("model/params.txt", "model/weights.h5")
 
     def model_api(input_string):
+        # remove extraneous beginning padding
         comma = input_string.find(',')
         input_data = input_string[comma + 1:]
-        print(input_data)
-        #new_string = ""
-        #for i in range(len(input_data)):
-        #    if input_data[i] == ' ':
-        #        new_string += '+'
-        #    else:
-        #        new_string += input_data[i]
-        #print(new_string)
+
+        # decode base64 data
         decoded_string = base64.b64decode(input_data)
         input_img = Image.open(BytesIO(decoded_string))
-        print(input_img.size)
 
+        # convert to greyscale
         greyscale = input_img.convert('L')
-        greyscale.save("greyscale.jpg")
-        print(greyscale.size)
 
-        # create numpy array and convert to greyscale
+        # convert to numpy array
         img = np.array(greyscale)
-        print(img.shape)
 
         # crop image to 100 x 100 square
         img = img[0:100, 0:100]
-        print(img.shape)
-        print(img)
 
         img = np.expand_dims(img, axis=0)
         img = np.expand_dims(img, axis=0)
-        print(img.shape)
 
-        # 3. call model predict function
+        # call model predict function
         pred1, pred2 = EntireModel("model/12_11_2018-model.h5", img)
 
-        # 5. return the output for the api
+        # return the output for the api
         return pred1, pred2
 
     return model_api
